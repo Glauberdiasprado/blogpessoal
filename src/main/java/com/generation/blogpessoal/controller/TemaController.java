49b284d948d8834105f0c43911eb189eda5c7478
Glauberdiasprado <glauberdiasprado@gmail.com>
2022-07-15 16:08:25 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -21,50 +21,53 @@ import com.generation.blogpessoal.model.Tema;
 import com.generation.blogpessoal.repository.TemaRepository;
 
 @RestController
+@RequestMapping("/temas")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping("/tema")
 public class TemaController {
 
 	@Autowired
-	private TemaRepository repository;
-	
+	private TemaRepository temaRepository;
+
 	@GetMapping
-	public ResponseEntity<List<Tema>> getAll(){
-		return ResponseEntity.ok(repository.findAll());
+	public ResponseEntity<List<Tema>> getAll() {
+		return ResponseEntity.ok(temaRepository.findAll());
+
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Tema> GetById(@PathVariable Long id){
-		return repository.findById(id)
-				.map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<Tema> getById(@PathVariable Long id) {
+		return temaRepository.findById(id)
+			.map(resposta -> ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.notFound().build());
 	}
-	
-	@GetMapping("/nome/{nome}")
-	public ResponseEntity<List<Tema>> GetByName(@PathVariable String nome){
-		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome));
+
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Tema>> getByDescricao(@PathVariable String descricao) {
+		return ResponseEntity.ok(temaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Tema> inserirTema(@Valid @RequestBody Tema tema){
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	public ResponseEntity<Tema> postTema(@Valid @RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Tema> atualizarTema (@Valid @RequestBody Tema tema){
-		
-		return repository.findById(tema.getId())
-				.map(reposta -> {
-					return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
+	public ResponseEntity<Tema> putTema(@Valid @RequestBody Tema tema) {
+					
+		return temaRepository.findById(tema.getId())
+				.map(resposta -> {
+					return ResponseEntity.ok().body(temaRepository.save(tema));
 				})
 				.orElse(ResponseEntity.notFound().build());
+
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteTema(@PathVariable Long id) {
 		
-		return repository.findById(id)
-				.map(reposta -> {
+		return temaRepository.findById(id)
+				.map(resposta -> {
+					temaRepository.deleteById(id);
 					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 				})
 				.orElse(ResponseEntity.notFound().build());
--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -1,73 +1,40 @@
 package com.generation.blogpessoal.controller;
 
-import java.util.List;
 import java.util.Optional;
-
-import javax.validation.Valid;
-
-import com.generation.blogpessoal.model.UserLogin;
-import com.generation.blogpessoal.model.Usuario;
-import com.generation.blogpessoal.repository.UsuarioRepository;
-import com.generation.blogpessoal.service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.UserLogin;
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "", allowedHeaders = "")
 public class UsuarioController {
 
-    /**
-     * Faz uma injeção de dependência da classe de Serviço UsuarioService
-     * para ter acesso aos métodos do CRUD com regras de negócio
-     */
     @Autowired
     private UsuarioService usuarioService;
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
-    @GetMapping("/all")
-    public ResponseEntity <List<Usuario>> getAll(){
-
-        return ResponseEntity.ok(usuarioRepository.findAll());
-
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
-        return usuarioRepository.findById(id)
-            .map(resposta -> ResponseEntity.ok(resposta))
-            .orElse(ResponseEntity.notFound().build());
-    }
-
-    
-    // PARTE DIFERENTE DO VIDEO ATUALIZADA
     @PostMapping("/logar")
-    public ResponseEntity<UserLogin> login(@RequestBody Optional<UserLogin> userLogin) {
-        return usuarioService.logarUsuario(userLogin)
-            .map(resposta -> ResponseEntity.ok(resposta))
-            .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+        return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 
     }
 
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Usuario> Post (@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(usuarioService.cadastrarUsuario(usuario));
 
-    @PutMapping("/atualizar")
-    public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
-        return usuarioService.atualizarUsuario(usuario)
-            .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
-            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-   
 }
